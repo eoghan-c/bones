27051a81c07e4ae1486d263bbb0bfd8a7989db3f
eoghan-c <eoghan_clarkson@yahoo.com>
2014-08-17 12:05:19 +0100

    Adapt 'Page' template
    Remove the sidebar & make ‘Page’ full-width, put Prev & Next buttons in
    Footer.

--- page.php
+++ page.php
@@ -4,7 +4,8 @@
 
 				<div id="inner-content" class="wrap cf">
 
-						<div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
+						<?php /* EC Don't show sidebar on 'pages' replacing classes 't-2of3 d-5of7' */ ?>
+						<div id="main" class="m-all t-all d-all cf" role="main">
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
@@ -13,10 +14,6 @@
 								<header class="article-header">
 
 									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
-
-									<p class="byline vcard">
-										<?php printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-									</p>
 
 								</header> <?php // end article header ?>
 
@@ -37,16 +34,43 @@
 										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
 										 *
 										*/
-										wp_link_pages( array(
+										/*wp_link_pages( array(
 											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
 											'after'       => '</div>',
 											'link_before' => '<span>',
 											'link_after'  => '</span>',
-										) );
+										) );*/
 									?>
 								</section> <?php // end article section ?>
 
 								<footer class="article-footer cf">
+
+									<?php
+										$pagelist = get_pages('sort_column=menu_order&sort_order=asc');
+										$pages = array();
+										foreach ($pagelist as $page) {
+										   $pages[] += $page->ID;
+										}
+
+										$current = array_search(get_the_ID(), $pages);
+										$prevID = $pages[$current-1];
+										$nextID = $pages[$current+1];
+									?>
+
+									<div id="rdsvs_opt_main_navigation">
+									<?php if (!empty($prevID)) { ?>
+											<a class="rdsvs-opt-previous rdsvs-opt-nav-button" 
+												href="<?php echo get_permalink($prevID); ?>"
+												data-opt_post_id="<?php echo $prevID; ?>"
+									  			title="<?php echo get_the_title($prevID); ?>">Previous</a>
+									<?php }
+									if (!empty($nextID)) { ?>
+											<a class="rdsvs-opt-next rdsvs-opt-nav-button" 
+												href="<?php echo get_permalink($nextID); ?>" 
+												data-opt_post_id="<?php echo $nextID; ?>"
+										 		title="<?php echo get_the_title($nextID); ?>">Next</a>
+									<?php } ?>
+									</div><!-- #rdsvs_opt_main_navigation -->
 
 								</footer>
 
@@ -72,7 +96,7 @@
 
 						</div>
 
-						<?php get_sidebar(); ?>
+						<?php /*get_sidebar(); */ /* Do not show the sidebar on 'pages' */?>
 
 				</div>
 
